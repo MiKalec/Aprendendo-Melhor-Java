fd6de513fefae609dd83a70a87b21d1baaa0b12c
MiKalec <msmikaelis@gmail.com>
2018-06-20 14:07:40 -0300

    Adicionando coisas aprendidas. Cap 13

--- src/br/com/caelum/contas/main/TestaString.java
+++ src/br/com/caelum/contas/main/TestaString.java
@@ -1,6 +1,13 @@
 package br.com.caelum.contas.main;
 
 public class TestaString {
+    //ex5 Cap 13
+    private void imprimeLinhaLinha(String s){
+        for(int i = 0; i<s.length();i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+
     public static void main(String[] args){
         String s = " fj11 ";
         String teste = s.replaceAll("1", "2");
@@ -13,5 +20,10 @@ public class TestaString {
         System.out.println(dentro);
         System.out.println(semEspaco);
         System.out.println(vazio);
+        System.out.println(s.length());
+
+        TestaString t = new TestaString();
+        t.imprimeLinhaLinha(semEspaco);
+
     }
 }
