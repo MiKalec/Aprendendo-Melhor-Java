85733e4318632aae499c816a0aedd163e7b8f467
MiKalec <msmikaelis@gmail.com>
2018-06-25 16:15:18 -0300

    Adicionando coisas aprendidas. Cap 16

---
+++ src/br/com/caelum/contas/main/TestaInteger.java
@@ -0,0 +1,14 @@
+package br.com.caelum.contas.main;
+
+public class TestaInteger {
+    public static void main(String[] args){
+        Integer x1 = 10;
+        Integer x2 = 10;
+
+        if(x1.equals(x2)/*x1 == x2*/){
+            System.out.println("igual");
+        }else{
+            System.out.println("não é");
+        }
+    }
+}
